3ba4dc08f8a25dc22350ac45d7eb6b038abc5251
Patrick Steiner <Patrick@Steiner-Buchholz.de>
2017-03-09 15:07:34 +0100

    manual merge

---
+++ Smart_Gateway/Docker_Files/scripts/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# echo "Copy $BUNDLE_NAME for hot deployment -> don't do that in production!"
+# cp $HOME/target/$BUNDLE_NAME $HOME/$FUSE_LOCATION/deploy
+
+# Start Fuse
+$HOME/$FUSE_LOCATION/bin/fuse server
